f0aac1007cd9b44d99e80b9721ab0e114d490718
Vibha Rawan <vibharawan12@gmail.com>
2021-04-14 10:41:41 +0530

    simple dynamic web page without use of database

---
+++ dynamic page without database/travello/App/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from .models import Destination
+# Create your views here.
+
+def index(request):
+    dest1=Destination()
+    dest1.name='Mumbai'
+    dest1.descrip='The City that never sleeps.'
+    dest1.price=600
+    dest1.img='destination_1.jpg'
+    
+    dest2=Destination()
+    dest2.name='Pune'
+    dest2.descrip='The place for all educational instittues'
+    dest2.price=700
+    dest2.img='destination_2.jpg'
+
+    dest3=Destination()
+    dest3.name='Thane'
+    dest3.descrip='Food Junction'
+    dest3.price=150
+    dest3.img='destination_3.jpg'
+
+
+    dests=[dest1,dest2,dest3]
+    return render(request,'index.html',{'dests':dests})
