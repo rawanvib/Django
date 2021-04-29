8a42f82a742cdfe170212fafd5a1224d562eb919
Vibha Rawan <vibharawan12@gmail.com>
2021-04-29 11:06:49 +0530

    added two functionality search and show dwatil page of an item. Need to work on url mapping.

--- product_category/shop/store/urls.py
+++ product_category/shop/store/urls.py
@@ -5,9 +5,10 @@ app_name='store'
 
 urlpatterns=[
     path('',views.home_page_view,name="index"),
-    path('<int:id>/',views.product_list,name='product_list'),
+    path('<int:pk>/',views.category_list,name='category_list'),
     path('register/',views.register_view,name="register"),
     path("login/",views.login,name="login"),
     path("logout/",views.logout,name="logout"),
-    path("detail-<int:id>",views.show_detail,name="detail")
+    path("detail-<int:id>/",views.show_detail,name="detail"),
+    path("search_dish",views.search_dish,name="search_dish"),
 ]
--- product_category/shop/store/views.py
+++ product_category/shop/store/views.py
@@ -1,40 +1,35 @@
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib import messages
-from django.shortcuts import render, redirect, get_object_or_404
-
+from django.contrib.auth.models import User,auth
 from .models import Product, Category
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def home_page_view(request):
-    # products=None
-    # categories=Category.objects.all()
-    # categoryId=request.GET.get("category")
-    # print(categoryId)
-    # if categoryId:
-    #    products=Product.objects.filter(category=categoryId)
-    # else:
-    # products=Product.objects.all()
-    #
-    # data={}
-    # data['products']=products
-    # data['categories']=categories
-    return render(request, 'store/index.html')
-
-
-def product_list(request, pk):
-    products = None
-    categories = Category.objects.all()
+def home_page_view(request,pk=None):
+    categories=Category.objects.all()
     products = Product.objects.all()
 
-    if pk:
-        category = get_object_or_404(Category, pk=pk)
-        products = Product.objects.filter(category=category)
+    data={}
+    data['products']=products
+    data['categories']=categories
+    return render(request, 'store/index.html',data)
 
-    data = {}
-    data['categories'] = categories
-    data['products'] = products
+def category_list(request,pk):
+    category = Category.objects.get(pk=pk)
+    products = Product.objects.filter(category=category)
+    categories=Category.objects.all()
+    return render(request, 'store/index.html', {'products': products, 'categories':categories})
 
-    return render(request, 'store/index.html', data)
+
+def search_dish(request):
+    if request.method == 'POST':
+        search=request.POST.get('searched')
+        products=Product.objects.filter(name__icontains = search)
+
+        return render(request, 'store/search.html', {'search' : search, 'products':products})
+    else:
+        return render(request, 'store/search.html', {})
+
 
 
 def register_view(request):
@@ -53,27 +48,25 @@ def register_view(request):
             elif User.objects.filter(email=email).exists():
                 messages.info(request, "email already taken")
             else:
-                user = User.objects.create_user(username=username, password=password1, email=email,
-                                                first_name=first_name, last_name=last_name)
+                user=User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
                 user.save()
                 return redirect("store:login")
         else:
-            messages.info(request, "password not matching")
+            messages.info(request,"password not matching")
             return redirect("store:register")
-        return redirect('/store')
+        return redirect("/store")
     else:
         return render(request, 'store/register.html')
 
-
 def login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
 
-        user = auth.authenticate(username=username, password=password)
+        user=auth.authenticate(username=username,password=password)
 
         if user is not None:
-            auth.login(request, user)
+            auth.login(request,user)
             messages.success(request, 'you are logged in')
             return redirect("/store")
         else:
@@ -81,15 +74,14 @@ def login(request):
             return redirect("store:login")
     else:
 
-        return render(request, 'store/login.html')
-
+        return render(request,'store/login.html')
 
 def logout(request):
     auth.logout(request)
     return redirect("/store")
 
 
-def show_detail(request, id):
-    product = Product.objects.get(id=id)
-    # product = get_object_or_404(Product,pk=id)
+def show_detail(request,id):
+    product=Product.objects.get(id=id)
+    #product = get_object_or_404(Product,pk=id)
     return render(request, "store/show_detail.html", {"product": product})
